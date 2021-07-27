918347a81cc5516b52306af925bc4e7e3aad06b8
Akash Pattnaik <akashjio66666@gmail.com>
2021-07-27 23:06:35 +0530

    init project

---
+++ README.md
@@ -0,0 +1,75 @@
+# <h1 align="center">🚀MailSenderBot📩</h1>
+# <p align="center"><a href="https://github.com/BLUE-DEVIL1134/IronEye"><img src="https://github-readme-stats.vercel.app/api/pin?username=BLUE-DEVIL1134&show_icons=true&theme=dracula&hide_border=true&repo=MailSenderBot"></a></p>
+<p align="center">
+<a href="https://github.com/BLUE-DEVIL1134/MailSenderBot"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FBLUE-DEVIL1134%2FMailSenderBot%2F&count_bg=%232100FF&title_bg=%2300BBFF&icon=github.svg&icon_color=%23000000&title=Views&edge_flat=false" /></a>
+<img src="https://img.shields.io/badge/Version-1.0.0-blueviolet?&logo=github&style=flat" />
+</p>
+
+# Setup
+First, we will setup the `Gmail Account`.
+
+<kbd>Step 1</kbd> • Goto [`This Link`](https://myaccount.google.com/u/0/lesssecureapps) and click on the `Allow less secure apps` button.
+
+<kbd>Step 2</kbd> • Open Gmail > Settings > Fowarding and POP / IMAP
+
+<kbd>Step 3</kbd> • Click on `Enable POP for all mail (even mail that's already been downloaded)` option.
+
+<kbd>Step 3</kbd> • Click on `Enable IMAP` option.
+
+<kbd>Step 4</kbd> • Click on `Save` button.
+
+
+# 🚀Deploy
+For Deploying the application, there are these following options available...
+
+• [`Local server`](#local-server)<br>
+• [`Qovery`](#qovery)<br>
+• [`Heroku`](#heroku)
+
+
+# <img height="32px" src="https://user-images.githubusercontent.com/593151/82310296-7c85ba80-99c4-11ea-88c2-8e6452885e6a.png" />Local Server
+To start the application locally, you can use the following command...
+
+• Set The [Environmental Variables](#environmental-variables)<br>
+>Set them either via editing the source or in your `OS` Env.
+```bash
+$ npm i
+$ npm build
+$ npm start
+```
+• Star the repository if you like it
+
+# <img height="32px" src="https://uploads-ssl.webflow.com/5de176bfd41c9b0a91bbb0a4/5df74bc4b0ebe7619e4cd55a_qovery_logo_square_dark_margin_32.png" />Qovery
+To deploy the application on Qovery, you can do the following...
+
+• Fork this repository<br>
+• Create a new application on Qovery<br>
+• Add the forked repository as a remote<br>
+• Set branch to `main`<br>
+• Set The [Environmental Variables](#environmental-variables)<br>
+• Deploy The Application<br>
+• Star the repository if you like it
+
+# <img height="32px" src="https://www.herokucdn.com/favicon.ico" />Heroku
+To deploy the application on Heroku, you can do the following...
+
+• Fork this repository<br>
+• Create a new application on Heroku<br>
+• Connect your github account to Heroku<br>
+• Add the forked repository as a remote<br>
+• Set branch to `main`<br>
+• Set The [Environmental Variables](#environmental-variables)<br>
+• Deploy The Application<br>
+• Star the repository if you like it
+
+# Environmental Variables
+The following environmental variables are required to deploy the application...
+
+• `botToken` botToken issued by [@BotFather](https://telegram.dog/BotFather)<br>
+• `userId` user_id of the user<br>
+• `login` The email of the user (Gmail only)<br>
+• `password` The password of the user
+
+# Credentials
+• [`Anony`](https://github.com/anonyindian) The bot idea was his.<br>
+• [`Akash`](https://github.com/BLUE-DEVIL1134) The bot was built by him.
---
+++ app/index.ts
@@ -0,0 +1,134 @@
+'use-strict';
+
+// @ts-ignore
+import notifier from 'mail-notifier';
+import { Markup, Telegraf } from 'telegraf';
+import fs from 'fs';
+import chalk from 'chalk';
+import { dirname } from 'node:path';
+
+if (
+  process.env.botToken == undefined ||
+  process.env.userId == undefined ||
+  process.env.login == undefined ||
+  process.env.password == undefined
+) {
+  console.error(
+    chalk.redBright(
+      `[ ${chalk.greenBright(new Date().toLocaleTimeString())} ] Error: Required Environmental Variables Not Present...`
+    ),
+  );
+  process.exit(0);
+}
+
+const bot = new Telegraf(process.env.botToken);
+
+const imap = {
+  user: process.env.login,
+  password: process.env.password,
+  host: "imap.gmail.com",
+  port: 993,
+  tls: true,
+  tlsOptions: { rejectUnauthorized: false }
+};
+
+// The Mail Server Object
+const mailServer = notifier(imap);
+
+// @ts-ignore
+mailServer.on('mail', (mail) => {
+  bot.telegram.sendMessage(
+    String(process.env.userId),
+    `📮 New Mail Recieved\n` +
+    `*🙍‍♂️From Name* • \`${mail.from[0].name}\`\n` +
+    `*From Mail* • \`${mail.from[0].address}\`\n` +
+    `*Subject* • \`${mail.subject}\`\n` +
+    `*📅Date* • \`${mail.date}\`\n` +
+    `*Priority* • \`${mail.priority}\`\n` +
+    `*💬Text* • \`${(mail.text != undefined || mail.text != '') ? mail.text?.length >= 176 ? mail.text.slice(0, 175) + '...+' : mail.text : "\`Couldn't Get That!\`"}\``,
+    {
+      parse_mode: 'Markdown',
+      reply_markup: Markup.inlineKeyboard([
+        [Markup.button.url('Open In Gmail 📧', `https://mail.google.com/mail/u/0/#search/rfc822msgid:${mail.messageId}`),],
+      ]).reply_markup,
+    }
+  ).catch((err) => {
+    console.error(
+      chalk.magentaBright(
+        `[ ${chalk.greenBright(new Date().toLocaleTimeString())} ] Error: Couldn't Send Message\nErrorMessage: ${err.message}`
+      ),
+    );
+  });
+});
+
+bot.command('start', async (ctx) => {
+  const masterUser = await bot.telegram.getChatMember(String(process.env.userId), Number(process.env.userId));
+  ctx.reply(
+    'Hi 👋\nI am MailServerBot](https://github.com/BLUE-DEVIL1134/MailServerBotn' +
+    'I am designed by Akash](https://github.com/BLUE-DEVIL1134/)**to send your emails directly to telegram.\n' +
+    `I am deployed by • ${masterUser.user.first_name}\n` +
+    'Currently, i only work for `Gmail`.',
+    {
+      parse_mode: 'Markdown',
+      reply_markup: Markup.inlineKeyboard([
+        [Markup.button.url('🚀 Deploy Your Own Bot', 'https://github.com/BLUE-DEVIL1134/MailServerBot'),],
+      ]).reply_markup,
+    }
+  );
+});
+
+bot.command('license', async (ctx) => {
+  ctx.replyWithDocument(
+    {
+      source: fs.createReadStream(dirname(__dirname) + '/LICENSE'),
+      filename: 'LICENSE',
+    },
+    {
+      parse_mode: 'Markdown',
+      caption: '*License* • `GPL-3.0-only`\n*Author • Akash Pattnaik* <`akashpattnak.github@gmail.com`>',
+    }
+  );
+});
+
+bot.command(['help', 'commands'], async (ctx) => {
+  ctx.reply(
+    '*These Are My Following Commands ->*\n\n' +
+    '/start • Start The Bot\n' +
+    '/commands • See This Message\n' +
+    '/uptime • To See My Uptime\n' +
+    '/license • Get My License',
+    {
+      parse_mode: 'Markdown',
+    }
+  );
+});
+
+bot.command('uptime', async (ctx) => {
+    const hours = Math.floor(process.uptime() / 3600);
+    const minutes = Math.floor((process.uptime() - hours * 3600) / 60);
+    const seconds = Math.floor(process.uptime() - hours * 3600 - minutes * 60);
+    const upTime = (hours > 0 ? hours + "h " : "") + (minutes > 0 ? minutes + "m " : "") + seconds + "s";
+
+  ctx.reply(`Hi 👋\n\nI am running for almost \`${upTime}\``, {parse_mode: 'Markdown'});
+});
+
+mailServer.on('connected', () => {
+  console.log(chalk.magentaBright(`[ ${chalk.greenBright(new Date().toLocaleTimeString())} ] Connected To The Mail Server...`));
+});
+
+mailServer.on('error', (error: Error) => {
+  console.error(chalk.redBright(`[ Error ]: ${chalk.red(error.message)}`));
+});
+
+mailServer.start().catch((error: Error) => {
+  console.error(chalk.redBright(`[ ${chalk.greenBright('Error')} ]: ${chalk.yellowBright(error.message)}`));
+});
+bot.launch().then(() => {
+  console.log(chalk.magentaBright(`[ ${chalk.greenBright(new Date().toLocaleTimeString())} ] Telegram Bot Started...`));
+}).catch((error: Error) => {
+  console.error(chalk.redBright(`[ ${chalk.greenBright('Error')} ]: ${chalk.yellowBright(error.message)}`));
+});
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
